e2dc140889bde514dcb788dd3d1a431878750f75
XTY <136148653+XTY64XTY12345@users.noreply.github.com>
2024-08-28 11:13:48 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,3 +7,14 @@ import './style.css'
 import './var.css'
 import './vp-code-group.css'
 import './custom-block.css'
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  }
+} satisfies Theme
