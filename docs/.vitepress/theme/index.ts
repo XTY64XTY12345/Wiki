f6769bd8728cea435d15aa5837ee0f4eda9e9f0a
XTY <136148653+XTY64XTY12345@users.noreply.github.com>
2024-11-09 09:35:33 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -10,11 +10,40 @@ import './custom-block.css'
 import './blockquote.css'
 import './rainbow.css'
 import Linkcard from "./components/Linkcard.vue"
+// giscus
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 
 export default {
   extends: DefaultTheme,
+  
   enhanceApp({app}) { 
     // 注册全局组件
     app.component('Linkcard' , Linkcard)
   },
+  
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+    const route = useRoute();
+        
+    // giscus配置
+    giscusTalk({
+      repo: 'XTY64XTY12345/Wiki', //仓库
+      repoId: 'R_kgDOMWWaig', //仓库ID
+      category: 'Announcements', // 讨论分类
+      categoryId: 'DIC_kwDOMWWais4CkG7I', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    );
 }
+
