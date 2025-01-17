6b3f30e77633d6422cdda8d8fcaece1eec305b16
XTY <136148653+XTY64XTY12345@users.noreply.github.com>
2025-01-17 17:05:27 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -19,6 +19,19 @@ import { onMounted, watch, nextTick } from 'vue';
 import { useRoute } from 'vitepress';
 // 时间线
 import "vitepress-markdown-timeline/dist/theme/index.css";
+import { NProgress } from 'nprogress-v2/dist/index.js' // 进度条组件
+import 'nprogress-v2/dist/index.css' // 进度条样式
+
+if (inBrowser) {
+      NProgress.configure({ showSpinner: false })
+      router.onBeforeRouteChange = () => {
+        NProgress.start() // 开始进度条
+      }
+      router.onAfterRouteChanged = () => {
+         busuanzi.fetch()
+         NProgress.done() // 停止进度条
+      }
+}
 
 export default {
   extends: DefaultTheme,
