43db244f6e434781f58b6830e78d3ffc321c3609
XTY <136148653+XTY64XTY12345@users.noreply.github.com>
2025-02-22 10:58:03 +0800

    Create Ecode.vue

---
+++ docs/.vitepress/theme/components/Ecode.vue
@@ -0,0 +1,22 @@
+<script src="./Ecode/ecode.js" setup>
+  
+interface Props {
+    desc: string
+    code: string
+}
+  
+const props = withDefaults(defineProps<Props>()， {
+    desc: ''，
+    code: ''，
+})
+
+</script>
+
+<template>
+    <div class="ecode" desc="{{ props.desc }}">{{ props.code }}
+    </div>
+</template>
+
+<style>
+@import url(./Ecode/ecode.css);
+</style>
