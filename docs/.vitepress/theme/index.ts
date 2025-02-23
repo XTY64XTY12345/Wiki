71a6baeb07afe10c341a7d167336fe798c69df65
XTY <136148653+XTY64XTY12345@users.noreply.github.com>
2025-02-23 14:59:27 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -12,10 +12,10 @@ import './rainbow.css'
 import Linkcard from "./components/Linkcard.vue"
 // giscus
 import giscusTalk from 'vitepress-plugin-comment-with-giscus';
-import { useData， useRoute } from 'vitepress';
+import { useData, useRoute } from 'vitepress';
 // 图片放大
 import mediumZoom from 'medium-zoom';
-import { onMounted， watch， nextTick } from 'vue';
+import { onMounted, watch, nextTick } from 'vue';
 import { useRoute } from 'vitepress';
 // 时间线
 import "vitepress-markdown-timeline/dist/theme/index.css";
@@ -24,11 +24,11 @@ import "vitepress-markdown-timeline/dist/theme/index.css";
 //import 'nprogress-v2/dist/index.css' // 进度条样式
 
 export 默认 {
-  extends: DefaultTheme，
+  extends: DefaultTheme,
   
   enhanceApp({app  }) { 
     // 注册全局组件
-    app。component('Linkcard' ， Linkcard) //链接卡片
+    app。component('Linkcard' , Linkcard) //链接卡片
 //      NProgress.configure({ showSpinner: false })
 //      router.onBeforeRouteChange = () => {
 //        NProgress.start() // 开始进度条
@@ -36,39 +36,39 @@ export 默认 {
 //      router.onAfterRouteChanged = () => {
 //         NProgress.done() // 停止进度条
 //      }
-  }，
+  },
  
   setup() {
     const route = useRoute();
     // 图片放大
     const initZoom = () => {
       // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
-      mediumZoom('.main img'， { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
     };
     onMounted(() => {
       initZoom();
     });
     watch(
-      () => route。path，
+      () => route。path,
       () => nextTick(() => initZoom())
     );
 
     // giscus配置
     const { frontmatter } = useData();
     giscusTalk({
-      repo: 'XTY64XTY12345/Wiki'， //仓库
-      repoId: 'R_kgDOMWWaig'， //仓库ID
-      category: 'Announcements'， // 讨论分类
-      categoryId: 'DIC_kwDOMWWais4CkG7I'， //讨论分类ID
-      mapping: 'pathname'，
-      inputPosition: 'bottom'，
-      lang: 'zh-CN'，
-      }， 
+      repo: 'XTY64XTY12345/Wiki', //仓库
+      repoId: 'R_kgDOMWWaig', //仓库ID
+      category: 'Announcements', // 讨论分类
+      categoryId: 'DIC_kwDOMWWais4CkG7I', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
       {
-        frontmatter， route
+        frontmatter, route
       },
-      //默认值为true，表示已启用，此参数可以忽略；
-      //如果为false，则表示未启用
+      //默认值为true,表示已启用,此参数可以忽略；
+      //如果为false,则表示未启用
       //您可以使用“comment:true”序言在页面上单独启用它
       true
     );
